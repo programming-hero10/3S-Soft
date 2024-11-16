cdefd908c1747da6f838c06e4fa52952bc4b0355
Mehady Hasan Akash <fivermehadyhasanakash@gmail.com>
2024-11-16 22:20:08 +0600

    fully animated hopeg and contact commit

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-
-
 // __________________________________________navigation____________________________________________________
 
-
-let x = document.getElementById('manu');
-let mIcon = document.getElementById('manuIcon');
-let cIcon = document.getElementById('closeIcon');
-
+let x = document.getElementById("manu");
+let mIcon = document.getElementById("manuIcon");
+let cIcon = document.getElementById("closeIcon");
 
 function openMenu() {
   x.style.display = "block";
@@ -23,7 +19,6 @@ function closeMenu() {
   mIcon.style.display = "block";
   cIcon.style.display = "none";
 }
-
 
 // __________________________________________FAQ____________________________________________________
 
@@ -44,3 +39,54 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// _________________Scroll effects________________________________________________
+window.addEventListener("scroll", () => {
+  const scrollWatcher = document.querySelector(".scroll-watcher");
+  const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+  const scrollPercentage = (window.scrollY / maxScroll) * 100;
+  scrollWatcher.style.width = `${scrollPercentage}%`;
+});
+//  opacity_________________
+document.addEventListener("scroll", () => {
+  const boxes = document.querySelectorAll(".box");
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+});
+document.addEventListener("scroll", () => {
+  const boxe = document.querySelectorAll(".boxe");
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxe.forEach((boxe) => {
+    const boxeTop = boxe.getBoundingClientRect().top;
+
+    if (boxeTop < triggerBottom) {
+      boxe.classList.add("show");
+    } else {
+      boxe.classList.remove("show");
+    }
+  });
+});
+document.addEventListener("scroll", () => {
+  const boxs = document.querySelectorAll(".boxs");
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxs.forEach((boxs) => {
+    const boxsTop = boxs.getBoundingClientRect().top;
+
+    if (boxsTop < triggerBottom) {
+      boxs.classList.add("show");
+    } else {
+      boxs.classList.remove("show");
+    }
+  });
+});

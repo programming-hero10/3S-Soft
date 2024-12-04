40b4f1563876142a2b088ff46e35d9d26dca4d02
Mehady Hasan Akash <fivermehadyhasanakash@gmail.com>
2024-12-04 19:24:21 +0600

    add advace contact form in contact page

--- index.js
+++ index.js
@@ -114,3 +114,23 @@ document.addEventListener("scroll", () => {
     }
   });
 });
+
+// _______________________________contact form js________________________________________
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
